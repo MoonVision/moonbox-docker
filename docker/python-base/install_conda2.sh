0c3c97437dfd89774215e8b63deadc326fd858cb
ahirner <a.hirner@gmail.com>
2021-03-06 21:17:55 +0100

    conda clean properly
    following: https://github.com/jupyter/docker-stacks/pull/867

--- docker/python-base/install_conda2.sh
+++ docker/python-base/install_conda2.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-conda clean -tipsy
+conda clean --all -f --yes
 conda install --yes python=3.6 \
                     mkl=2019.4 \
                     mkl-include=2019.4 \
@@ -13,7 +13,7 @@ conda install --yes -c pytorch protobuf
 conda install --yes -c conda-forge numpy=1.19.4 \
                                     pybind11 \
                                     blas
-conda clean --all --yes
+conda clean --all -f --yes
 pip install setuptools \
     opencv-python-headless==4.2.0.32 \
     opencv-contrib-python-headless==4.2.0.32
