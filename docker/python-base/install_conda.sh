958977f7e09451b404bdd352b9d222d23cb439e4
ahirner <a.hirner@gmail.com>
2021-10-23 16:43:03 +0200

    commit to mkl
    $ readlink -e /opt/conda/lib/python3.8/site-packages/numpy/core/../../../../libcblas.so
    /opt/conda/lib/libmkl_rt.so.1

--- docker/python-base/install_conda.sh
+++ docker/python-base/install_conda.sh
@@ -16,6 +16,9 @@ rm Mambaforge-$(uname)-$(uname -m).sh
 eval "$(~/mambaforge/bin/conda shell.bash hook)"
 conda init bash
 
+# commit to mkl on upgrades
+echo ""blas=*=mkl"" >> ~/mambaforge/conda-meta/pinned
+
 echo "Updating conda base environment..."
 # downgrades to py 3.8 just fine
 conda env update -f /bd_build/environment.yml
