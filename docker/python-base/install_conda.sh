d7a5b53b6feb7f2c6509d1ec62bb1f0406c2f891
ahirner <a.hirner@gmail.com>
2021-10-23 14:12:59 +0200

    bump base miniconda

--- docker/python-base/install_conda.sh
+++ docker/python-base/install_conda.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-wget -q https://repo.anaconda.com/miniconda/Miniconda3-py38_4.9.2-Linux-x86_64.sh -O ~/miniconda.sh
+wget -q https://repo.anaconda.com/miniconda/Miniconda3-py38_4.10.3-Linux-x86_64.sh -O ~/miniconda.sh
 /bin/bash ~/miniconda.sh -b -p /opt/conda
 rm ~/miniconda.sh
 ln -s /opt/conda/etc/profile.d/conda.sh /etc/profile.d/conda.sh
