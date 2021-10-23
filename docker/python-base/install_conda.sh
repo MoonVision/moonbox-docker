d38067f5582697bfdd1ed669d6bab6e6605e0dff
ahirner <a.hirner@gmail.com>
2021-10-23 16:53:35 +0200

    use previous /opt/conda location

--- docker/python-base/install_conda.sh
+++ docker/python-base/install_conda.sh
@@ -2,29 +2,29 @@
 set -euo pipefail
 
 curl -L -O https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh
-# intro accept, accept license, install at default location, don't run conda init
+# intro accept, accept license, install at historical conda location, don't run conda init
 bash Mambaforge-$(uname)-$(uname -m).sh << EOF
 
 yes
-
+/opt/conda
 no
 EOF
 
 rm Mambaforge-$(uname)-$(uname -m).sh
 
 # activate in current and future sessions
-eval "$(~/mambaforge/bin/conda shell.bash hook)"
+eval "$(/opt/conda/bin/conda shell.bash hook)"
 conda init bash
 
 # commit to mkl on upgrades
-echo ""blas=*=mkl"" >> ~/mambaforge/conda-meta/pinned
+echo ""blas=*=mkl"" >> /opt/conda/conda-meta/pinned
 
 echo "Updating conda base environment..."
 # downgrades to py 3.8 just fine
 conda env update -f /bd_build/environment.yml
 
 conda clean --all -f --yes
-find ~/mambaforge -type f,l -name '*.a' -delete
-find ~/mambaforge -type f,l -name '*.pyc' -delete
-find ~/mambaforge -type f,l -name '*.js.map' -delete
+find /opt/conda/ -type f,l -name '*.a' -delete
+find /opt/conda/ -type f,l -name '*.pyc' -delete
+find /opt/conda/ -type f,l -name '*.js.map' -delete
 pip cache purge
