220444c8e9daad9d562182dec31fb9a92d3e1b1c
ahirner <a.hirner@gmail.com>
2022-11-20 14:34:11 +0100

    micromamba needs explicit channel

--- docker/moonbox/install_pylon.sh
+++ docker/moonbox/install_pylon.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 echo Install Pylon
 
-micromamba install swig -y
+micromamba install swig -y -c conda-forge
 
 curl -o /tmp/pylon.deb https://www.baslerweb.com/fp-1551786503/media/downloads/software/pylon_software/pylon_5.2.0.13457-deb0_amd64.deb
 dpkg -i /tmp/pylon.deb
